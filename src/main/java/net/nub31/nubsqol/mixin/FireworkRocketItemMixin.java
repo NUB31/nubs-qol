ea8e5e53698a94b394674dddb99d4033734244f8
NUB31 <oliverlhs@outlook.com>
2023-08-14 21:02:45 +0200

    added checking for wearing elytra when launching

--- src/main/java/net/nub31/nubsqol/mixin/FireworkRocketItemMixin.java
+++ src/main/java/net/nub31/nubsqol/mixin/FireworkRocketItemMixin.java
@@ -1,6 +1,8 @@
 package net.nub31.nubsqol.mixin;
 
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ElytraItem;
 import net.minecraft.item.FireworkRocketItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
@@ -15,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class FireworkRocketItemMixin {
     @Inject(at = @At("HEAD"), method = "use")
     private void useItem(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
-        if (user.isSneaking() && !user.isFallFlying()) {
+        if (user.getEquippedStack(EquipmentSlot.CHEST).getItem() instanceof ElytraItem && user.isSneaking() && !user.isFallFlying()) {
             if (world.isClient) user.jump();
             // Has to be done on the server unfortunately
             if (!world.isClient) user.startFallFlying();
